150885206113fc0b8b25e1788d4c16cf3ccdbc5c
Sebastian Postigo <sebastian.postigo@tektonlabs.com>
2020-06-08 17:43:44 -0500

    expenses handle error

--- models/Expense.js
+++ models/Expense.js
@@ -1,11 +1,11 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose, { Schema } from "mongoose";
 
 const expenseSchema = mongoose.Schema({
-    description: { type: String, required: true },
-    amount: { type: Number, required: true, default: 0 },
-    created_at: { type: Date, required: true, default: Date.now() },
-    account: { type: Schema.Types.ObjectId, ref:'Account', required: true },
-    category: { type: Schema.Types.ObjectId, ref:'Category', required: true }
+  description: { type: String, required: true },
+  amount: { type: Number, required: true, default: 0 },
+  created_at: { type: Date, required: true, default: Date.now() },
+  // account: { type: Schema.Types.ObjectId, ref:'Account', required: true },
+  // category: { type: Schema.Types.ObjectId, ref:'Category', required: true }
 });
 
-export default mongoose.model('Expense', expenseSchema);
+export default mongoose.model("Expense", expenseSchema);
--- routes/expenses.js
+++ routes/expenses.js
@@ -1,41 +1,41 @@
-import { Router } from 'express'; 
-import ExpenseService from '../services/ExpenseService';
-import Expense from '../models/Expense';
+import { Router } from "express";
+import ExpenseService from "../services/ExpenseService";
+import Expense from "../models/Expense";
 const expenseServiceInstance = new ExpenseService(Expense);
 
 export default (app) => {
-    const route  = Router();
-    app.use('/expenses', route);
+  const route = Router();
+  app.use("/expenses", route);
 
-    route.get('/', async (req, res, next) => {
-        try {
-            const { expenses } = await expenseServiceInstance.getAll();
+  route.get("/", async (req, res, next) => {
+    try {
+      const { expenses } = await expenseServiceInstance.getAll();
 
-            res.json(expenses);
-        } catch (error) {
-            next(error);
-        }
-    });
-    
-    route.post('/', async (req, res, next) => {
-        try {
-            const expenseDTO = req.body;
-            const { expense } = await expenseServiceInstance.add(expenseDTO);
+      res.json(expenses);
+    } catch (error) {
+      next(error);
+    }
+  });
 
-            res.json(expense);
-        } catch (error) {
-            next(error);
-        }
-    });
+  route.post("/", async (req, res, next) => {
+    try {
+      const expenseDTO = req.body;
+      const { expense } = await expenseServiceInstance.add(expenseDTO);
 
-    route.delete('/:id', async (req, res, next) => {
-        try {
-            const id = req.params.id;
-            const { expense } = await expenseServiceInstance.delete(id);
+      res.json(expense);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  });
 
-            res.json(expense);
-        } catch (error) {
-            next(error);
-        }
-    });
-}
+  route.delete("/:id", async (req, res, next) => {
+    try {
+      const id = req.params.id;
+      const { expense } = await expenseServiceInstance.delete(id);
+
+      res.json(expense);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  });
+};
